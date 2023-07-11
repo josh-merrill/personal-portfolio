78d3d06a724ba92faafe7f4a0c507813570cd11a
Josh Merrill <joshmmerrill@outlook.com>
2023-07-10 17:34:10 -0700

    STYLE: Updating experience component styling

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,9 +4,10 @@ import {AiFillHtml5} from 'react-icons/ai'
 import {DiCss3} from 'react-icons/di'
 import {DiJavascript1} from 'react-icons/di'
 import {SiReact} from 'react-icons/si'
+import {SiTailwindcss} from 'react-icons/si'
 import {DiBootstrap} from 'react-icons/di'
+import {SiWebflow} from 'react-icons/si'
 import {FiFigma} from 'react-icons/fi'
-import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
@@ -15,53 +16,56 @@ const Experience = () => {
         <h2>Experience</h2>
       </div>
       <div className="container experience__container">
-        {/* <div className="experience__frontend"> */}
-          {/* <h3>Frontend Development</h3> */}
           <div className="experience__content">
             <article className="experience__details">
               <AiFillHtml5 className='experience__details-icon' />
               <div>
                 <h3>HTML</h3>
-                {/* <small className='text-dark'>Experienced</small> */}
               </div>
             </article>
             <article className="experience__details">
               <DiCss3 className='experience__details-icon' />
               <div>
                 <h3>CSS</h3>
-                {/* <small className='text-dark'>Experienced</small> */}
               </div>
             </article>
             <article className="experience__details">
               <DiJavascript1 className='experience__details-icon' />
               <div>
                 <h3>JavaScript</h3>
-                {/* <small className='text-dark'>Experienced</small> */}
               </div>
             </article>
             <article className="experience__details">
               <SiReact className='experience__details-icon' />
               <div>
                 <h3>React</h3>
-                {/* <small className='text-dark'>Intermediate</small> */}
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiTailwindcss className='experience__details-icon' />
+              <div>
+                <h3>Tailwind CSS</h3>
               </div>
             </article>
             <article className="experience__details">
               <DiBootstrap className='experience__details-icon' />
               <div>
                 <h3>Bootstrap</h3>
-                {/* <small className='text-dark'>Experienced</small> */}
               </div>
             </article>
             <article className="experience__details">
               <FiFigma className='experience__details-icon' />
               <div>
                 <h3>Figma</h3>
-                {/* <small className='text-dark'>Experienced</small> */}
+              </div>
+            </article>
+            <article className="experience__details">
+              <SiWebflow className='experience__details-icon' />
+              <div>
+                <h3>Webflow</h3>
               </div>
             </article>
           </div>
-        {/* </div> */}
       </div>
     </section>
   )
